5855b6950b205eb7ebadaf2fb3974b85225b4e05
Marianoarsad <arsadmariano@yahoo.com.ph>
2025-09-25 22:59:04 +0800

    Added header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,39 @@
+import AmrahLogo from '../assets/amrah-logo.png';
+import UserIcon from '/user.svg';
+import ShoppingBag from '/shopping-bag.svg'
+import Search from '/magnifying-glass.svg';
+
+export default function Header ({  }) {
+    
+    return (
+        <header id='header'>
+            {/* LOGO  */}
+            <a>
+                <img src={AmrahLogo} alt="amrah-logo" width="140rem"/>
+            </a>
+            <nav>
+                <ul>
+                    <li><a href='#'>Home</a></li>
+                    <li><a href='#'>Shop</a></li>
+                    <li><a href='#'>Collections</a></li>
+                    <li><a href='#'>Services</a></li>
+                    
+                </ul>
+                
+            </nav>
+
+            {/* ICONS  */}
+            <p>
+                    <a>
+                        <img src={Search}  width="30rem"/>
+                    </a>
+                    <a>
+                        <img src={ShoppingBag}  width="30rem"/>
+                    </a>
+                    <a>
+                        <img src={UserIcon}  width="30rem"/>
+                    </a>    
+            </p>
+        </header>
+    )
+}
---
+++ src/components/Promo.jsx
@@ -0,0 +1,8 @@
+export default function Promo ({}) {
+
+    return (
+        <div id='promo'>
+                <p><strong>Enjoy ₱1500 off on your #GlitterGift purchase, use code GLITTERGIFT1500</strong></p>
+        </div>
+    )
+}
