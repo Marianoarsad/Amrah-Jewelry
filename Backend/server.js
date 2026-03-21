3ca232ddffe6301a573c0c4a9535e7160d483570
Marianoarsad <arsadmariano@yahoo.com.ph>
2026-03-21 16:00:59 +0800

    Added Shop Component

---
+++ Backend/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+const port = 3000;
+const API_URL = 'http://localhost:3000';
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
+
+app.get("/", (req, res) => {
+    res.status(200);
+});
